d423590c8397ead6ed4fde7b085596b214cdfd8a
ldevernay <boblehamster@yahoo.fr>
2018-12-04 15:13:57 +0100

    first user unit tests

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,12 +1,18 @@
 import { expect } from 'chai';
 import { createConnection } from 'typeorm';
 import User from '../src/entity/User';
-import { create } from 'domain';
 
 describe('User CRUD', () => {
-    it('properly creates the user', () => {
-        createConnection().then (async connection => {
-            console.log(await User.find());
-        })
+    it('properly creates the user', async () => {
+        return createConnection().then(async connection => {
+            const user = new User();
+            user.name = "Bobby";
+            await user.save();
+
+            let result = await User.find({name: "Bobby"});
+            if (result){
+                expect(result.length).to.be.greaterThan(0);
+            }
+        });
     })
 })
