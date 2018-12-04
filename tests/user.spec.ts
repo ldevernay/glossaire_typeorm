3369b24eb1e2689b1cdc4eaa8bcc807d88480268
ldevernay <boblehamster@yahoo.fr>
2018-12-04 15:58:59 +0100

    handle db connection properly while testing

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -3,8 +3,12 @@ import { createConnection } from 'typeorm';
 import User from '../src/entity/User';
 
 describe('User CRUD', () => {
+    let connection;
+    before(async () => {
+        connection = await createConnection();
+    })
     it('properly creates the user', async () => {
-        return createConnection().then(async connection => {
+        //return createConnection().then(async connection => {
             const user = new User();
             user.name = "Bobby";
             await user.save();
@@ -13,6 +17,22 @@ describe('User CRUD', () => {
             if (result){
                 expect(result.length).to.be.greaterThan(0);
             }
-        });
+        //});
+    });
+    it('successfully updates a given user', async () => {
+        //return createConnection().then(async connection => {
+            const bobby = await User.findOne({name: "Bobby"});
+            bobby.name = "John";
+            await bobby.save();
+
+            let result = await User.findOne({name: "John"});
+            if (result){
+                expect(result).to.exist;
+            }
+        //})
     })
-})
+
+    after(async () => {
+        await connection.close();
+    })
+});
